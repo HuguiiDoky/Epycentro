783a84a97cfd4fe8f33ad73c159e3a1f77ff0f47
Angel <a23010228@uhipocrates.edu.mx>
2025-12-08 10:12:39 -0600

    feat: Archivo de Avances de las Graficas

---
+++ graficas.py
@@ -0,0 +1,14 @@
+# graficas.py - VERSIÓN DÍA 1
+import matplotlib.pyplot as plt
+
+def configurar_estilo_visual():
+    """Configura los estilos globales de las gráficas para el modo oscuro."""
+    plt.style.use('dark_background')
+    plt.rcParams['figure.facecolor'] = '#0E1117'
+    plt.rcParams['axes.facecolor'] = '#0E1117'
+    plt.rcParams['text.color'] = 'white'
+    return "Estilos visuales configurados correctamente."
+
+def obtener_placeholder_mapa():
+    """Genera una imagen vacía o texto para ocupar espacio en la UI."""
+    return "Aquí se visualizará el mapa de calor en lal fase 3."
